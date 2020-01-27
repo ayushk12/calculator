5438d56990d3391dbe3ce88a97bbaac7f1e583c6
ayush kumar <kayush242@gmail.com>
2020-01-27 21:16:14 +0530

    done

--- script.js
+++ script.js
@@ -5,13 +5,14 @@ var operand2 = null;
 var operator = null;
 var evaluated = false;
 
+//evaluate check any string value and convert into number 
 function evaluate(value) {
     if (Number.isInteger(+value)) {
         display.textContent = "";
     }
     evaluated = false;
 }
-
+//thi function holds all the event 
 function eventHandler(value) {
     if (
         value === "+" ||
@@ -40,7 +41,7 @@ function eventHandler(value) {
         display.textContent = "";
     }
 }
-
+//add event listener for all the buttons
 for (var i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
         var value = this.getAttribute("data-value");
@@ -50,7 +51,7 @@ for (var i = 0; i < buttons.length; i++) {
         eventHandler(value);
     });
 }
-
+//using addeventListener to keyboard key event
 document.addEventListener("keydown", function (event) {
     var value = event.key;
     console.log(value);
